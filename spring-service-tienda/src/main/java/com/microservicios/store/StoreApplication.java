d1075e57963be86e1a30736551e8ddd00332962c
ArturoVa <85954822+ArturoVa@users.noreply.github.com>
2024-03-25 12:24:53 -0600

    Se corrigen errores

--- spring-service-tienda/src/main/java/com/microservicios/store/StoreApplication.java
+++ spring-service-tienda/src/main/java/com/microservicios/store/StoreApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-//@EnableDiscoveryClient // como es el mas nuevo no funciona con eureka
 @EnableFeignClients
 public class StoreApplication {
 
