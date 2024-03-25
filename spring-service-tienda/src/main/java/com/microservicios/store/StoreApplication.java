ee922c805a449bc6b390460610bbead93983028b
ArturoVa <85954822+ArturoVa@users.noreply.github.com>
2024-03-25 11:27:55 -0600

    Se crea servicio de tienda

---
+++ spring-service-tienda/src/main/java/com/microservicios/store/StoreApplication.java
@@ -0,0 +1,17 @@
+package com.microservicios.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+//@EnableDiscoveryClient // como es el mas nuevo no funciona con eureka
+@EnableFeignClients
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+	}
+
+}
