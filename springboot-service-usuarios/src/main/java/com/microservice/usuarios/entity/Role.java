2b9c05c92da9939100b746c01afb15fba3f0ee91
ArturoVa <85954822+ArturoVa@users.noreply.github.com>
2024-05-04 13:15:47 -0600

    Se finaliza entidades de usuario y roles

--- springboot-service-usuarios/src/main/java/com/microservice/usuarios/entity/Role.java
+++ springboot-service-usuarios/src/main/java/com/microservice/usuarios/entity/Role.java
@@ -1,5 +1,6 @@
 package com.microservice.usuarios.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -15,5 +16,22 @@ public class Role {
 	private Long id;
 	
 	@Column(unique = true, length = 30)
+	private String nombre;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	} 
 	
 }
--- springboot-service-usuarios/src/main/java/com/microservice/usuarios/entity/Usuario.java
+++ springboot-service-usuarios/src/main/java/com/microservice/usuarios/entity/Usuario.java
@@ -1,13 +1,19 @@
 package com.microservice.usuarios.entity;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(name="usuarios")
@@ -29,8 +35,17 @@ public class Usuario implements Serializable {
 	private Boolean enabled;
 	private String nombre;
 	private String apellido;
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "usuarios_to_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "rooles_id"), uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "rooles_id"})})
+	private List<Role> rooles;
 	
 	
+	public List<Role> getRoles() {
+		return rooles;
+	}
+	public void setRoles(List<Role> roles) {
+		this.rooles = roles;
+	}
 	public Long getId() {
 		return id;
 	}
