e4b3db87ea4cb2d3a2cc925ccec8b92ab9272292
ArturoVa <85954822+ArturoVa@users.noreply.github.com>
2024-05-05 14:28:31 -0600

    Se agrega servicio de oauth

--- springboot-service-usuarios/src/main/resources/import.sql
+++ springboot-service-usuarios/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios (username, password, enabled, nombre , apellido, email) VALUES ('artu','12345', 1, 'Arturo', 'Vazquez','artva@gmail.com');
-INSERT INTO usuarios (username, password, enabled, nombre , apellido, email) VALUES ('migueeel','12345', 1, 'Miguel', 'Lopez','milop@gmail.com');
+INSERT INTO usuarios (username, password, enabled, nombre , apellido, email) VALUES ('artu','$2a$10$/EXqJGrSnkPj6xuywf0r1uALU6nyovGfU1KTOWz8Mbp9kuoE25ma6', 1, 'Arturo', 'Vazquez','artva@gmail.com');
+INSERT INTO usuarios (username, password, enabled, nombre , apellido, email) VALUES ('migueeel','$2a$10$W7ejfWXlfJRIBQ.7Ibo.d.KTz9/qWkUhiaDW/.m8OF9J8UwK.R.3K', 1, 'Miguel', 'Lopez','milop@gmail.com');
 
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USER');
